7a14234b9a9910a3b6173c8da63a9ba9a86f750f
Darren Seet Serkai <darren@seet.me>
2014-07-03 11:21:30 +0900

    Added Handlebars template

---
+++ chapter3/meadowlark/site/meadowlark.js
@@ -0,0 +1,42 @@
+/**
+ * Created by dseet on 7/3/2014.
+ */
+var express = require('express');
+
+var app = express();
+
+// set up handlebars view engine
+var handlebars = require('express3-handlebars')
+    .create({ defaultLayout: 'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.get('/about', function (req, res) {
+    res.render('about');
+});
+
+// 404 catch-all handler (middleware)
+app.use(function (req, res) {
+    res.status(404);
+    res.render('404');
+});
+
+// 500 error handler (middleware)
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
+});
+
+var port = app.get('port');
+app.listen(port, function () {
+    console.log('Express stated on http://localhost:' + port + '; press Ctrl-C to terminate.');
+});
