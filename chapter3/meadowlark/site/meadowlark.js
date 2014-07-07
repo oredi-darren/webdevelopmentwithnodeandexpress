bb1d565cba27cf69d3a71c53a53cf94607691b02
Darren Seet Serkai <darren@seet.me>
2014-07-07 15:41:49 +0900

    Complete chapter 7 Templating with Handlebars

--- chapter3/meadowlark/site/meadowlark.js
+++ chapter3/meadowlark/site/meadowlark.js
@@ -7,8 +7,45 @@ var fortune = require('./lib/fortune.js');
 
 var app = express();
 
+function getWeatherData() {
+    return {
+        locations: [
+            {
+                name: 'Portland',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+                weather: 'Overcast',
+                temp: '54.1 F (12.3 C)'
+            },
+            {
+                name: 'Bend',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+                weather: 'Partly Cloudy',
+                temp: '55.0 F (12.8 C)'
+            },
+            {
+                name: 'Manzanita',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+                weather: 'Light Rain',
+                temp: '55.0 F (12.8 C)'
+            }
+        ]
+    };
+}
+
 // set up handlebars view engine
-app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
+app.engine('handlebars', handlebars({
+    defaultLayout: 'main',
+    helpers: {
+        section: function (name, options) {
+            if(!this._sections) this._sections = {};
+            this._sections[name] = options.fn(this);
+            return null;
+        }
+    }
+}));
 app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
@@ -17,6 +54,12 @@ app.use(express.static(__dirname + '/public'));
 
 app.use(function (req, res, next) {
     res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+    next();
+});
+
+app.use(function (req, res, next) {
+    if(!res.locals.partials) res.locals.partials = {};
+    res.locals.partials.weather = getWeatherData();
     next();
 });
 
@@ -43,6 +86,22 @@ app.get('/tours/oregon-coast', function (req, res) {
 app.get('/tours/request-group-rate', function (req, res) {
     res.render('tours/request-group-rate');
 });
+
+app.get('/nursery-rhyme', function (req, res) {
+    res.render('nursery-rhyme');
+});
+
+app.get('/data/nursery-rhyme', function (req, res) {
+    res.json({
+        animal: 'squirrel',
+        bodyPart: 'tail',
+        adjective: 'bushy',
+        noun: 'heck'
+    });
+});
+
+
+
 
 // 404 catch-all handler (middleware)
 app.use(function (req, res) {
